c325229209f6155270966e9693c0ec7620649284
Kevin Baumgartner <kevin.baumgartner@aon.at>
2025-12-29 18:24:15 +0100

    add logging module and replaced print statements with log messages, add gitignore file for log.txt

--- renameit.py
+++ renameit.py
@@ -1,5 +1,21 @@
 # renameit script
 import os
+import logging
+import sys
+
+# logger config
+logger = logging.getLogger(__name__)
+stream_handler = logging.StreamHandler(sys.stdout)
+formatter_stream = logging.Formatter("[%(asctime)s] {%(levelname)s} - %(message)s")
+stream_handler.setFormatter(formatter_stream)
+logger.addHandler(stream_handler)
+file_handler = logging.FileHandler('log.txt')
+formatter_file = logging.Formatter("[%(asctime)s] {%(levelname)s} %(name)s: #%(lineno)d - %(message)s")
+file_handler.setFormatter(formatter_file)
+logger.addHandler(file_handler)
+
+# set the logging level
+logger.setLevel(logging.WARNING)
 
 # main function
 def main():
@@ -18,7 +34,7 @@ def main():
         elif user_input.lower() == "simple":
             simple_rename()
         else:
-            print("Invalid input. Enter help for info.\n")
+            logger.warning(f"Invalid Input: {user_input}")
 
 # print welcome
 def print_header(message):
@@ -39,9 +55,9 @@ def print_help():
 # simple rename function
 def simple_rename():
     fotos_path = get_path()
-    print(fotos_path)
+    logger.debug(f"Path to pictures: {fotos_path}")
     pictures = get_file_names(fotos_path)
-    print(pictures)
+    logger.debug(f"Found pictures: {pictures}")
     pass
 
 # ask user for path to directory with pictures
@@ -54,7 +70,9 @@ def get_path():
             return user_input_path
         else:
             failed_cnt += 1
+            logger.info(f"Invalid Input for path: {user_input_path}. Nr. of tries: {failed_cnt}")
             if failed_cnt > 3:
+                logger.info(f"Path input was invalid {failed_cnt} times.")
                 return None
 
 # 
@@ -65,13 +83,13 @@ def get_file_names(path):
     for file in os.listdir(path):
         if file.lower().count('.heic') > 0:
             pic_type_heic_cnt += 1
-            print(file)
+            logger.debug(f"Valid .heic file found: {file}")
             pictures.append(file)
         elif file.lower().count('.jpg') > 0:
             pic_type_jpg_cnt += 1
-            print(file)
+            logger.debug(f"Valid .jpg file found: {file}")
             pictures.append(file)
-    
+    logger.debug(f"{pic_type_heic_cnt} .HEIC Files, {pic_type_jpg_cnt} .JPG Files")
     return pictures
 
 
